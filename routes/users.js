b30231aafab22cbd541f6d273fedef2a77c73595
Nkrumah <nkrumahthis@gmail.com>
2022-06-24 04:43:18 +0000

    add post and get users with in-memory data

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const users = [];
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send(users);
+});
+
+/* GET user by id. */
+router.get('/:id', function (req, res, next) {
+  res.send(users[req.params.id]);
+});
+
+/* POST new user. */
+router.post('/', function (req, res, next) {
+  const id = users.length;
+  user = { id: id, ...req.body };
+  users.push(user);
+  res.send(user);
 });
 
 module.exports = router;
